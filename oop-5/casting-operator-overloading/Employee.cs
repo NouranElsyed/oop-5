5ce7f5670c12bc0ff1117d53dc10f5e635b27378
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-22 19:14:29 +0300

    Casting Operator Overloading

---
+++ oop-5/casting-operator-overloading/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_5.casting_operator_overloading
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public Guid SecurityStamp { get; set; }
+
+    }
+}
