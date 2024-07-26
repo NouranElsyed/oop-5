27943c0885a1b76987c36ff8025293a9593f7649
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-26 10:43:04 +0300

    modifed

--- secondProject/Program.cs
+++ secondProject/Program.cs
@@ -6,12 +6,10 @@
         {
             //Define Class Maths that has four methods: Add, Subtract, Multiply, and Divide, each of them takes two parameters.Call each method in Main().
             // Modify the program so that you do not have to create an instance of class to call the four methods.
-            Maths result = new Maths();
-    
-            Console.WriteLine(result.Add(2, 5));
-            Console.WriteLine(result.Subtract(2, 5));
-            Console.WriteLine(result.Multiply(2, 5));
-            Console.WriteLine(result.Divide(2, 5));
+            Console.WriteLine(Maths.Add(2, 5));
+            Console.WriteLine(Maths.Subtract(2, 5));
+            Console.WriteLine(Maths.Multiply(2, 5));
+            Console.WriteLine(Maths.Divide(2, 5));
 
 
 
